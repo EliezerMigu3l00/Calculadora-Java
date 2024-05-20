5191947968e48f078195e5d602f916ea0fbc660f
EliezerMigu3l00 <eliezermiguel97@outlook.com>
2024-05-20 11:37:43 -0300

    Funcionalidades da calculadora pronta

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,2 +1,16 @@
 public class Calculadora {
+    int resultadoInt;
+    double resultadoDouble;
+    public int somar(int valorA, int valorB){
+        return resultadoInt = valorA + valorB;
+    }
+    public int subitrair (int valorA, int valorB) {
+        return resultadoInt = valorA - valorB;
+    }
+    public int dividir (int valorA, int valorB){
+        return resultadoInt = valorA / valorB;
+    }
+    public double divisaoNumerosFlutuantes (double valorA, double valorB) {
+        return  resultadoDouble = valorA / valorB;
+    }
 }
