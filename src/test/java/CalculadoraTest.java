ad181f14984450ddfb763cc4d02f24da5add4ed0
EliezerMigu3l00 <eliezermiguel97@outlook.com>
2024-05-21 10:02:11 -0300

    Testes unitarios finalizados

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,2 +1,53 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculadoraTest {
+    @Test
+    public void deveSomarCorretamenteDoisNumerosInteiros() {
+        //Arrange
+        int valorA = 2;
+        int valorB = 2;
+        int valorEsperado = 4;
+        Calculadora calculadora = new Calculadora();
+        //Act
+        int valorCalculado = calculadora.somar(valorA, valorB);
+        //Assert
+        Assert.assertEquals(valorEsperado,valorCalculado, 0.01);
+    }
+    @Test
+    public void deveSubitrairCorretamenteDoisNumerosInteiros() {
+        //Arrange
+        int valorA = 5;
+        int valorB = 2;
+        int valorEsperado = 3;
+        Calculadora calculadora = new Calculadora();
+        //Act
+        int valorCalculado = calculadora.subitrair(valorA, valorB);
+        //Assert
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
+    }
+    @Test
+    public void deveDividirCorretamenteDoisNumerosInteiros() {
+        //Arrange
+        int valorA = 10;
+        int valorB = 2;
+        int valorEsperado = 5;
+        Calculadora calculadora = new Calculadora();
+        //Act
+        int valorCalculado = calculadora.dividir(valorA, valorB);
+        //Assert
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
+    }
+    @Test
+    public void deveSubtrairCorretamenteQuandoOsNumerosForemDePontoFlutuante() {
+        //Arrange
+        double valorA = 12.5;
+        double valorB = 2;
+        double valorEsperado = 6.25;
+        Calculadora calculadora = new Calculadora();
+        //Act
+        double valorCalculado = calculadora.divisaoNumerosFlutuantes(valorA, valorB);
+        //Assert
+        Assert.assertEquals(valorEsperado, valorCalculado, 0.01);
+    }
 }
